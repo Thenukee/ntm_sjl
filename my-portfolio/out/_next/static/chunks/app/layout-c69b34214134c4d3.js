b64797521a8bdb5d68bf5fde77890d83b86496b0
Lean Beef Patty <123946674+Thenukee@users.noreply.github.com>
2025-07-24 16:07:11 +0530

    out vidible

---
+++ my-portfolio/out/_next/static/chunks/app/layout-c69b34214134c4d3.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{279:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,8065,23))},8065:()=>{}},e=>{e.O(0,[526,738,30,358],()=>e(e.s=279)),_N_E=e.O()}]);
